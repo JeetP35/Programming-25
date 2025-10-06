9e6cb633a257419da1272ab469900a73f29a0cf6
JeetP35 <jeetpadhya35@gmail.com>
2025-10-05 22:00:03 -0600

    Update IteratingPi.py

--- Practice Test/IteratingPi.py
+++ Practice Test/IteratingPi.py
@@ -3,12 +3,12 @@
 def pi():
     n = 0
     piList = []
-    tolerance = 1e-10
-    while True:
+    piSum = 0
+    while piSum != 3.141592653:
         pi = ((-1)**n) / ((2*n) + 1)
         piList.append(pi)
         n += 1
         piSum = 4 * sum(piList)
-        if abs(piSum - 3.141592653589793) < tolerance:
-            print(f"Calculated value of pi: {piSum}")
+    print(piSum)
+
 pi()
