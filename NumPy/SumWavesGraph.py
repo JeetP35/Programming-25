736847417d2233afb4f710e70130f7d591658a16
JeetP35 <jeetpadhya35@gmail.com>
2025-10-22 11:12:30 -0600

    Update SumWavesGraph.py

--- NumPy/SumWavesGraph.py
+++ NumPy/SumWavesGraph.py
@@ -2,4 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def generate_wave(n, xMin, xMax, iterations=1000):
+    x = np.array()
+    y = np.array()
+    a = np.array()
+    b = np.array()
     return
