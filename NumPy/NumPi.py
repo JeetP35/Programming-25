58fde00af0e8e9b917e6501a22ad38875232b12f
JeetP35 <jeetpadhya35@gmail.com>
2025-10-22 11:12:27 -0600

    Update NumPi.py

--- NumPy/NumPi.py
+++ NumPy/NumPi.py
@@ -1,25 +1,12 @@
 import numpy as np
-import math
+# import math
 
 def estimatePi(points):
     x = np.random.random(points)
     y = np.random.random(points)
     inside = (x**2 + y**2) <= 1
-    pi = 4 * np.sum(inside) / points
-    return pi
+    pi_est = 4 * np.sum(inside) / points
+    print(f"Estimated value of Pi with {points} points: {pi_est}")
+    return
 
-def nextEstimate(points, accuracy):
-    pi_est = estimatePi(points)
-    difference = abs(pi_est - math.pi)
-    if difference <= accuracy:
-        return points, pi_est
-    else:
-        return nextEstimate(points * 2, accuracy)
-
-def main():
-    accuracy = 1e-8
-    points = 1000
-    points, pi_est = nextEstimate(points, accuracy)
-    print(f"\nFinal π ≈ {pi_est} with {points} points (accuracy ±{accuracy})")
-
-main()
+estimatePi(100000000)
