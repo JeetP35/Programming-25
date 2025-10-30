a213a02eada02640c1efbda4574814eee5998afd
JeetP35 <jeetpadhya35@gmail.com>
2025-10-30 14:13:07 -0600

    Update SumWavesGraph.py

--- NumPy/SumWavesGraph.py
+++ NumPy/SumWavesGraph.py
@@ -1,9 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_wave(n, xMin, xMax, iterations=1000):
-    x = np.array()
-    y = np.array()
-    a = np.array()
-    b = np.array()
-    return
+def generateRandomWave(n, xMin, xMax, iter = 1000):
+    x = np.linspace(xMin, xMax, iter)
+    a = np.random.rand(n)
+    b = np.random.rand(n)
+    y = np.zeros((n, iter))
+
+    for i in range(n):
+        y[i] = a[i] * np.sin(b[i] * x)
+        plt.plot(x, y[i])
+
+    plt.savefig('waves.png')
+    plt.clf()
+
+    y_sum = np.sum(y, axis = 0)
+    plt.plot(x, y_sum)
+    plt.savefig('resultantWave.png')
+
+generateRandomWave(5, 0, 100)
