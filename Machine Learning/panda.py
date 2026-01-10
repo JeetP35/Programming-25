7ff4dd2d96e9f5e613106ea1fbcfed7b26e3dd43
JeetP35 <jeetpadhya35@gmail.com>
2026-01-10 11:48:34 -0700

    Update panda.py

--- Machine Learning/panda.py
+++ Machine Learning/panda.py
@@ -8,12 +8,32 @@ exam_scores = df['exam_score']
 study_hours = df['study_hours_per_day']
 sleep_hours = df['sleep_hours']
 
-print(exam_scores)
-
+#Part 1
 plt.scatter(study_hours,exam_scores)
-plt.savefig('GraphA.png')
+plt.savefig('examScore_and_studyHours.png')
 
-plt.clf()
+plt.clf() #Clearing Figure
 
 plt.scatter(sleep_hours,exam_scores)
-plt.savefig('GraphB.png')   
+plt.savefig('examScore_and_sleepHours.png')
+
+#Part 2
+def calculate(min_hours):
+  min_hours = float(min_hours)
+  if min_hours < 0:
+    print("Error: study hours cannot be negative")
+    return
+  
+  student = df[study_hours >= min_hours]
+
+  if len(student) == 0:
+    print(f"no student studied for that {min_hours} hours")
+    return
+  passed_students = df[(study_hours >= min_hours) & (exam_scores > 50)]
+  success_percentage = (len(passed_students) / len(student)) * 100
+  return success_percentage
+
+hours = input("Enter the number of study hours : ")
+result = calculate(hours)
+if result is not None:
+  print(f"{result}% of students who study for atleast {hours} hours a day passed the exam.")
