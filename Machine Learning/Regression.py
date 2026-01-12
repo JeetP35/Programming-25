b36dd104e14a5cd040dcbe3a78fbba1427f8c924
JeetP35 <jeetpadhya35@gmail.com>
2026-01-12 10:52:48 -0700

    Update Regression.py

--- Machine Learning/Regression.py
+++ Machine Learning/Regression.py
@@ -2,31 +2,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#Load Data
 df = pd.read_csv('student_habits_performance.csv')
 
+x = df['study_hours_per_day'].to_numpy()
+y = df['exam_score'].to_numpy()
 
-x = np.array([1, 2, 2.5, 3, 4, 4.5, 5, 6, 7, 8, 9])
-y_data = np.array([33, 41, 45, 51, 62, 68, 74, 80, 85, 90, 97])
-b = 30
-m = 7
+#values
+m = 0.0
+b = 0.0
 h = 1e-5
+rate = 0.001
+tolerance = 1e-6
 
+#predicting y
 def ypredict(x,m,b):
   return m * x + b
 
+#calculating mse
 def mse(y_data,y_pred):
   return np.mean((y_data - y_pred)**2)
 
-mse_init = mse(y_data, ypredict(x,m,b))
+y_predict = ypredict(x,m,b)
+mse_value = mse(y,y_predict)
 
-mse_nudge_m = mse(y_data, ypredict(x,m+h,b))
-mse_nudge_b = mse(y_data, ypredict(x,m,b+h))
+while True:
+  mse_nudge_m = mse(y, ypredict(x, m+h, b))
+  mse_nudge_b = mse(y, ypredict(x, m, b+h))
 
-m_slope = (mse_nudge_m - mse_init)/h
-b_slope = (mse_nudge_b - mse_init)/h
+  slope_m = (mse_nudge_m - mse_value) / h
+  slope_b = (mse_nudge_b - mse_value) / h
+  m -= rate * slope_m
+  b -= rate * slope_b
 
-print('initial mse is:', mse_init)
-print('mse after m is nudged by h is:', mse_nudge_m)
-print('mse after b is nudged by h is:', mse_nudge_b)
-print('slope along the m axis is:', m_slope)
-print('slope along the b axis is:',b_slope)
+  new_y_predict = ypredict(x, m, b)
+
+  new_mse = mse(y, new_y_predict)
+
+  if abs(mse_value - new_mse) < tolerance:
+    break
+
+  mse_value = new_mse
+
+print("Final b value:", b)
+print("Final m value:", m)
+print("Final mse", new_mse)
+
+#plotting final result
+plt.scatter(x,y)
+plt.plot(x, ypredict(x,m,b))
+plt.xlabel("study_hours_per_day")
+plt.ylabel("exam_score")
+# plt.ylim(0,100)
+plt.savefig("Exam Score vs Study Hours")
+plt.clf()
+plt.plot(x, ypredict(x,m,b))
+plt.xlabel("study_hours_per_day")
+plt.ylabel("exam_score")
+# plt.ylim(0,100)
+plt.savefig("Exam Score vs Study Hours (line of best fit)")
